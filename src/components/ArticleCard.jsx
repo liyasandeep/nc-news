b06c1904083625356df94d23de9038712f26c576
liyasandeep <liyasandeep1@gmail.com>
2022-11-03 23:40:34 +0000

    alert message given for article vote updation failure

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -3,80 +3,79 @@ import { FaRegCommentAlt } from "react-icons/fa";
 import { BiUpvote } from "react-icons/bi";
 import { BiDownvote } from "react-icons/bi";
 import * as API from "../api";
-import { useEffect, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import { useState } from "react";
 
 const ArticleCard = ({ article }) => {
   const [voteChangeValue, setVoteChangeValue] = useState(0);
   const handleVoteIncreaseClick = (article_id) => {
     setVoteChangeValue((currVote) => currVote + 1);
 
-    API.updateArticleVote(article_id, 1)
-      .then((article) => {
-        console.log(article.votes);
-      })
-      .catch((err) => {
-        setVoteChangeValue((currVote) => currVote - 1);
-      });
+    API.updateArticleVote(article_id, 1).catch((err) => {
+      setVoteChangeValue((currVote) => currVote - 1);
+      toast.error("OOPS!!!, Something went wrong, please try again later");
+    });
   };
 
   const handleVoteDecreaseClick = (article_id) => {
     setVoteChangeValue((currVote) => currVote - 1);
 
-    API.updateArticleVote(article_id, -1)
-      .then((article) => {
-        console.log(article.votes);
-      })
-      .catch((err) => {
-        setVoteChangeValue((currVote) => currVote + 1);
-        // setErr("OOPS!!!, Something went wrong, please try again later");
-      });
+    API.updateArticleVote(article_id, -1).catch((err) => {
+      setVoteChangeValue((currVote) => currVote + 1);
+      toast.error("OOPS!!!, Something went wrong, please try again later");
+    });
   };
 
   return (
-    <li className="articlecard">
-      <p className="topic">
-        In{" "}
-        <Link to={`/topics/${article.topic}`} className="link black">
-          <strong> {article.topic}</strong>
-        </Link>
-      </p>
-      <p className="postedBy">
-        Posted by <strong>{article.author}</strong> on{" "}
-        {new Date(article.created_at).toDateString()}{" "}
-      </p>
-      <Link to={`/articles/${article.article_id}`} className="link black">
-        <h3 className="article-title">{article.title}</h3>
-      </Link>
-
-      <div className="comment-vote-container">
-        <span className="comment">
-          <FaRegCommentAlt /> {article.comment_count}
-          <Link to={`/articles/${article.article_id}`} className="link black">
-            {" "}
-            <strong>Comments</strong>
-          </Link>
-        </span>
-        <span>
-          {article.votes + voteChangeValue} <strong>Votes</strong>
-        </span>
-
-        <button
-          disabled={voteChangeValue === 1}
-          onClick={() => handleVoteIncreaseClick(article.article_id)}
-          className="votebtn"
-        >
-          <BiUpvote />
-        </button>
-
-        <button
-          disabled={voteChangeValue === -1}
-          onClick={() => handleVoteDecreaseClick(article.article_id)}
-          className="votebtn"
-        >
-          <BiDownvote />
-        </button>
+    <>
+      <div>
+        <Toaster position="top-center" reverseOrder={false} />
       </div>
-    </li>
+      <li className="articlecard">
+        <p className="topic">
+          In{" "}
+          <Link to={`/topics/${article.topic}`} className="link black">
+            <strong> {article.topic}</strong>
+          </Link>
+        </p>
+        <p className="postedBy">
+          Posted by <strong>{article.author}</strong> on{" "}
+          {new Date(article.created_at).toDateString()}{" "}
+        </p>
+        <Link to={`/articles/${article.article_id}`} className="link black">
+          <h3 className="article-title">{article.title}</h3>
+        </Link>
+
+        <div className="comment-vote-container">
+          <span className="comment">
+            <FaRegCommentAlt /> {article.comment_count}
+            <Link to={`/articles/${article.article_id}`} className="link black">
+              {" "}
+              <strong>Comments</strong>
+            </Link>
+          </span>
+          <span>
+            {article.votes + voteChangeValue} <strong>Votes</strong>
+          </span>
+
+          <button
+            disabled={voteChangeValue === 1}
+            onClick={() => handleVoteIncreaseClick(article.article_id)}
+            className="votebtn"
+          >
+            <BiUpvote />
+          </button>
+
+          <button
+            disabled={voteChangeValue === -1}
+            onClick={() => handleVoteDecreaseClick(article.article_id)}
+            className="votebtn"
+          >
+            <BiDownvote />
+          </button>
+        </div>
+      </li>
+    </>
   );
 };
 export default ArticleCard;
