28fbe01b39b7adbcf950bce577f1900948bd85f3
liyasandeep <liyasandeep1@gmail.com>
2022-11-04 00:18:45 +0000

    errors handled

--- src/components/ListAllArticles.jsx
+++ src/components/ListAllArticles.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
-import { useParams, useSearchParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import * as API from "../api";
 import ArticleCard from "../components/ArticleCard";
 import SortAndOrderSection from "./SortAndOrderSection";
+import ErrorPage from "./ErrorPage";
 
 const ListAllArticles = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [articleList, setArticleList] = useState([]);
+  const [error, setError] = useState(null);
   const [selectSortValue, setSelectSortValue] = useState("created_at");
   const [selectOrderValue, setSelectOrderValue] = useState("desc");
 
@@ -14,39 +16,40 @@ const ListAllArticles = () => {
 
   useEffect(() => {
     setIsLoading(true);
-
-    API.getAllArticles(topicName, selectSortValue, selectOrderValue).then(
-      (articles) => {
+    API.getAllArticles(topicName, selectSortValue, selectOrderValue)
+      .then((articles) => {
         setArticleList(articles);
         setIsLoading(false);
-      }
-    );
+      })
+      .catch((err) => {
+        const {
+          response: {
+            data: { message },
+            status,
+          },
+        } = err;
+
+        setError({ message, status });
+        setIsLoading(false);
+      });
   }, [topicName, selectSortValue, selectOrderValue]);
   return isLoading ? (
     <p>Loading ...</p>
+  ) : error ? (
+    <ErrorPage message={error.message} status={error.status} />
   ) : (
     <section className="article-list">
       {topicName ? (
-        <>
-          <h2> Articles in {`${topicName}`}</h2>
-          <SortAndOrderSection
-            selectSortValue={selectSortValue}
-            setSelectSortValue={setSelectSortValue}
-            selectOrderValue={selectOrderValue}
-            setSelectOrderValue={setSelectOrderValue}
-          />
-        </>
+        <h2> Articles in {`${topicName}`}</h2>
       ) : (
-        <>
-          <h2>All Articles</h2>
-          <SortAndOrderSection
-            selectSortValue={selectSortValue}
-            setSelectSortValue={setSelectSortValue}
-            selectOrderValue={selectOrderValue}
-            setSelectOrderValue={setSelectOrderValue}
-          />
-        </>
+        <h2>All Articles</h2>
       )}
+      <SortAndOrderSection
+        selectSortValue={selectSortValue}
+        setSelectSortValue={setSelectSortValue}
+        selectOrderValue={selectOrderValue}
+        setSelectOrderValue={setSelectOrderValue}
+      />
       <ul>
         {articleList.map((article) => {
           return <ArticleCard key={article.article_id} article={article} />;
